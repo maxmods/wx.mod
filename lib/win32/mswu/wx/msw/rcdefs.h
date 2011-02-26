b596f6774ec857f0f532ab057443a68b53f36025
woollybah <woollybah@971f8453-5c39-0410-a452-3998bf019487>
2011-02-26 22:51:26 +0000

    Update to wxWidgets 2.9.1.
    git-svn-id: http://wxmax.googlecode.com/svn/trunk/wx.mod@688 971f8453-5c39-0410-a452-3998bf019487

--- lib/win32/mswu/wx/msw/rcdefs.h
+++ lib/win32/mswu/wx/msw/rcdefs.h
@@ -1,6 +1,6 @@
 # 1 "..\\..\\include\\wx\\msw\\genrcdefs.h"
 # 1 "<built-in>"
-# 1 "<command line>"
+# 1 "<command-line>"
 # 1 "..\\..\\include\\wx\\msw\\genrcdefs.h"
 # 12 "..\\..\\include\\wx\\msw\\genrcdefs.h"
  #ifndef _WX_RCDEFS_H
