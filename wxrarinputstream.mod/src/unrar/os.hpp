ef100e5e9b12e043afbfa705d9079f59b99ab167
woollybah <woollybah@971f8453-5c39-0410-a452-3998bf019487>
2015-02-15 18:15:06 +0000

    Fixed Windows build issue.
    git-svn-id: http://wxmax.googlecode.com/svn/trunk/wx.mod@857 971f8453-5c39-0410-a452-3998bf019487

--- wxrarinputstream.mod/src/unrar/os.hpp
+++ wxrarinputstream.mod/src/unrar/os.hpp
@@ -23,8 +23,8 @@
   #define STRICT
   #undef WINVER
   #undef _WIN32_WINNT
-  #define WINVER 0x0400
-  #define _WIN32_WINNT 0x0300
+  #define WINVER 0x0500
+  #define _WIN32_WINNT 0x0400
 
 
 #define WIN32_LEAN_AND_MEAN
